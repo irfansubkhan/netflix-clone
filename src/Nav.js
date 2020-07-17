30b087da7aeb1af76e0c44e5a1a7835b3e1861b5
irfansubkhan <irfansubkhan@gmail.com>
2020-07-17 14:41:31 +0700

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
